32d0d8e4f755079d096b4cebea946fb7720eca20
sebastienlestrat <sebastien.le-strat@hotmail.fr>
2023-05-03 14:34:04 +0200

    Etape 4 - done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import { EnfantComponent } from './enfant/enfant.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnfantComponent,
+    PokemonDetailsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/pokemon.ts
@@ -0,0 +1,7 @@
+export interface Pokemon {
+    id : number;
+    name : string;
+    type : string;
+    shortDescription : string;
+    longDescription : string;
+}
